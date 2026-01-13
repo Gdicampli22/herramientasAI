3dd8aa98d61d62fcff37ed332d902414368470dd
Gdicampli22 <149901517+Gdicampli22@users.noreply.github.com>
2026-01-13 20:40:54 -0300

    v8
    Cambios API_URL

--- pages/_Optimizador_Borradores.py
+++ pages/_Optimizador_Borradores.py
@@ -4,7 +4,10 @@ from PIL import Image
 
 # --- CONFIGURACIÓN ---
 st.set_page_config(page_title="AI Optimizador", page_icon="✍️", layout="centered")
-API_URL = "http://127.0.0.1:8000"
+if "RENDER_URL" in st.secrets:
+    API_URL = st.secrets["https://herramientasai.onrender.com/"]
+else:
+    API_URL = "http://127.0.0.1:8000"
 YOUR_NAME = "Gastón Di Campli" # <--- Pon tu nombre
 
 # --- HEADER & LOGO ---
